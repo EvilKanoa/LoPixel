1285e9e6fde0dbed960523c16d99231490ea1342
EvilKanoa <kanoa@kanoa.ca>
2013-09-18 13:40:32 -0400

    Started logging system

---
+++ src/ca/kanoa/lopixel/logging/Log.java
@@ -0,0 +1,21 @@
+package ca.kanoa.lopixel.logging;
+
+public class Log {
+	
+	public static void stats(String message) {
+		
+	}
+	
+	public static void info(String message) {
+		
+	}
+	
+	public static void warning(String message) {
+		
+	}
+	
+	public static void error(String message) {
+		
+	}
+	
+}
